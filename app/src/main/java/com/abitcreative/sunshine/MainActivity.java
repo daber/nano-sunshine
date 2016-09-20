c8c2a7a59a0a3510551e6b629742bf0e37d2755f
Michal Dabrowski <m.a.dabrowski@gmail.com>
2016-09-20 21:45:16 +0200

    Maps intent

--- app/src/main/java/com/abitcreative/sunshine/MainActivity.java
+++ app/src/main/java/com/abitcreative/sunshine/MainActivity.java
@@ -1,6 +1,8 @@
 package com.abitcreative.sunshine;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -18,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        if (id == R.id.action_showLocation) {
+            String location = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.key_location), getString(R.string.location_default_value));
+            Uri uri = Uri.parse("geo:0,0").buildUpon().appendQueryParameter("q", location).build();
+            Intent i = new Intent(Intent.ACTION_VIEW, uri);
+
+            if (i.resolveActivity(getPackageManager()) != null) {
+                startActivity(i);
+            }
             return true;
         }
         return super.onOptionsItemSelected(item);
